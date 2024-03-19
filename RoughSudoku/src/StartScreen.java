c5d383e4a999032cef098029c97eadc090f19e02
floydbrett <51729027+floydbrett@users.noreply.github.com>
2024-03-18 21:46:36 -0500

    Add files via upload

---
+++ RoughSudoku/src/StartScreen.java
@@ -0,0 +1,16 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class StartScreen extends JFrame{
+	
+	StartScreen() {
+		
+		JButton start = new JButton();
+		this.add(start);
+		
+		
+		
+		
+	}
+
+}
